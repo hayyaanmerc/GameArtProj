01516ceb1acd034f67bab4e9dcb631292c7860c4
hayyanmerc <abdulmerc7927@gmail.com>
2017-10-11 18:51:01 -0500

    arm drop

---
+++ Assets/ArmMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class ArmMove : MonoBehaviour
+{
+
+    public float speed = 1.5f;
+    private Vector3 target;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
