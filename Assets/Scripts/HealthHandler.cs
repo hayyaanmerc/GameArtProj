756c74d1b2eab20ee1dee646942a5c910062cc78
Fresh <fresh1736@gmail.com>
2017-10-16 15:42:08 -0500

    Broken Code

--- Assets/Scripts/HealthHandler.cs
+++ Assets/Scripts/HealthHandler.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//using Respawn;
+
 public class HealthHandler : MonoBehaviour {
 
 	public Movement playerInfo;
-	//public Slider healthSlider;
-	public Sprite healthSlider; 
-	public float maxHP = 10;
+	public Slider healthSlider;
+	public Respawn spawner;
+	//public Sprite healthSlider; 
+	//public float maxHP = playerInfo.GetComponent<Movement>().maxHP;
 	public float curHP;
 	// Use this for initialization
 	void Start () {
-		//healthSlider = GetComponent<Slider> ();
-		
+		healthSlider = GetComponent<Slider> ();
+		spawner =  GetComponent<Respawn> ();
 		playerInfo = GetComponent<Movement> ();
-		healthSlider = GetComponent<Sprite> ();
-		//healthSlider.maxValue = maxHP;
+		//healthSlider = GetComponent<Sprite> ();
+		healthSlider = playerInfo.GetComponent<Movement>();
 		//healthSlider.pivot = new Vector2(0,0);
-		curHP = maxHP;
-		//healthSlider.minValue = 0;
+		curHP = healthSlider.maxValue;
+		healthSlider.minValue = 0;
 		//healthSlider.anchorMax = maxHP;
-		//healthSlider.value = curHP;
+		healthSlider.value = curHP;
 
 
 
@@ -36,8 +39,17 @@ public class HealthHandler : MonoBehaviour {
 			if (curHP < 0)
 				curHP = 0;
 			//transform.localScale.x = curHP;
-			//healthSlider.value = curHP;
+			healthSlider.value = playerInfo.curHP;
+			if (curHP == 0) {
+				print ("You died");
+			}
 		}
 
+
+
 	}
+	public void Hit(){
+		curHP = curHP - 1;
+	}
+
 }
