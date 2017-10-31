ac345619f1f95df54b44f9a927fa0e43585a9373
Ijeri Omitogun <32554410+ijeriomit@users.noreply.github.com>
2017-10-31 00:07:57 -0500

    Prototype

---
+++ Assets/shop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shop : MonoBehaviour
+{
+
+    public void Shop_Open()
+    {
+        Application.LoadLevel("shop");
+    }
+}
